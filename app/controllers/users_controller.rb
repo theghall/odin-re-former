a8c19106781e574ebb62063e5981c723d3439b4f
Gary Hall <guategary@gmail.com>
2017-11-09 22:20:08 -0500

    Add edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,25 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    user.save
+    if user.save
+      redirect_to new_user_path
+    else
+      @user = user
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params['id'])
+  end
+
+  def update
+    @user = User.find_by(id: params['id'])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
 
